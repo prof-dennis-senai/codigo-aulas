6b19255e8d3ee9c6b93c6eb14d07e2b85ead5f09
Dennis Rocha <dennis0201@gmail.com>
2024-04-01 22:08:09 -0300

    Update

--- aula-4/site_app/urls.py
+++ aula-4/site_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import home, produtos, precos
+from .views import home, produtos, precos, deletar, atualizar
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
+
+    path('deletar/<int:id>', deletar, name="deletar"),
+    path('atualizar/<int:id>', atualizar, name="atualizar"),
+
     path('produtos/', produtos),
     path('precos/', precos),
     path('precos/<int:id>', precos),
--- aula-4/site_app/views.py
+++ aula-4/site_app/views.py
@@ -1,8 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+dados = []
 
 # Create your views here.
 def home(request):
-    return render(request, "site_app/global/home.html")
+    global dados
+    nome = ""
+    email = ""
+
+    if request.POST:
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        dados.append({"nome":nome,
+                    "email":email})
+        
+    for i,row in enumerate(dados):
+        row["id"] = i
+        
+    return render(request, "site_app/global/home.html", context={"dados":dados,"nome":nome,"email":email})
+
+
+def deletar(request,id):
+    global dados
+    dados.pop(id)
+    return redirect(home)
+
+def atualizar(request,id):
+    global dados
+    
+    if request.POST:
+        r = dados.pop(id)
+        print(r)
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        dados.insert(id,{"nome":nome,
+                    "email":email})
+
+        return redirect(home)
+    
+    nome = dados[id].get("nome")
+    email = dados[id].get("email")
+    return render(request, "site_app/global/atualizar.html", context={"id":id, "nome":nome,"email":email})
+
 
 
 def produtos(request):
